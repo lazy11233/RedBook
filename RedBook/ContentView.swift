8d1309c2a5215ee8f2d447afe4654e896bea60b4
liuzhao <1257625996@qq.com>
2024-04-22 00:45:04 +0800

    Text和Image的使用

--- RedBook/ContentView.swift
+++ RedBook/ContentView.swift
@@ -2,13 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(alignment: .center) {
+                // 静态的Text
+                Text("Hello Swift!")
         }
-        .padding()
     }
 }
 
