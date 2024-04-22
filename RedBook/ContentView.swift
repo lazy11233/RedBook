5d1721e708d4df78fa90c72bac4bf236ce900889
liuzhao <1257625996@qq.com>
2024-04-23 00:15:55 +0800

    加载显示图片

--- RedBook/ContentView.swift
+++ RedBook/ContentView.swift
@@ -1,10 +1,25 @@
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
+    @State var image: UIImage?
     var body: some View {
-            VStack(alignment: .center) {
-                // 静态的Text
-                Text("Hello Swift!")
+        VStack {
+            PhotosPicker("选择一张图片", selection: $selectedItem, matching: .images)
+                .onChange(of: selectedItem) { oldValue, newValue in
+                    Task {
+                        if let data = try? await selectedItem?.loadTransferable(type: Data.self) {
+                            image = UIImage(data: data)
+                        }
+                        print("加载图片失败")
+                    }
+                }
+            if let image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
         }
     }
 }
